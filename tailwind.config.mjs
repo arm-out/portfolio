e31d2152cad3932d21e91d22fbe9643e232693fa
arm-out <arminsuraj@gmail.com>
2024-02-29 13:31:27 -0800

    fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ["ABC Monument Grotesk", ...defaultTheme.fontFamily.sans],
+			},
 			colors: {
 				dark: "#0f0f0f",
 				fg: "#faf7ff",
