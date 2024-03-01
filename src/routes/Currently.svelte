cc5c6960ffd6440a66c13b9a686ae101ec12828d
arm-out <arminsuraj@gmail.com>
2024-02-29 20:46:24 -0800

    spotify api

---
+++ src/routes/Currently.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let song: string;
+	export let artist: string;
+	export let songLink: string;
+	let book: string = 'More Than This';
+	let author: string = 'Patrick Ness';
+	let bookLink: string = 'https://literal.club/armout/book/ness-patrickmore-than-this-97ga4';
+</script>
+
+<p>
+	Currently listening to <a
+		class="underline decoration-arm decoration-1 underline-offset-[3px] hover:bg-arm bg-arm/30"
+		href={songLink}
+		target="_blank">{song}</a
+	>
+	by {artist}
+	and slowly reading
+	<a
+		href={bookLink}
+		class="underline decoration-arm decoration-1 underline-offset-[3px] hover:bg-arm bg-arm/30"
+		target="_blank">{book}</a
+	>
+	by {author}
+</p>
