dca8f145e1c7e9d2d758bd21d4851f679f71b090
arm-out <arminsuraj@gmail.com>
2024-02-29 20:15:22 -0800

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['ABC Monument Grotesk', ...fontFamily.sans]
+			},
+			colors: {
+				dark: '#0f0f0f',
+				fg: '#faf7ff',
+				sec: '#626262',
+				outline: '#1d1d1d',
+				arm: '#6D28D9'
+			}
+		}
+	},
+	plugins: []
+};
