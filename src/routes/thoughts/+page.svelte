554d99219315d3b7e391ff7d4562f761f5fe584f
arm-out <arminsuraj@gmail.com>
2024-02-29 21:13:00 -0800

    scaffolding

--- src/routes/thoughts/+page.svelte
+++ src/routes/thoughts/+page.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
 	import { navPath } from '$lib/store';
 
-	navPath.set('/projects');
+	navPath.set('/thoughts');
 </script>
 
 <svelte:head>
-	<title>Projects</title>
+	<title>Thoughts</title>
 	<meta
 		name="description"
 		content="Studying Computer Science at the University of California, San Diego. Passionate about technology, music, and design. Consistently improving through learning."
 	/>
 </svelte:head>
 
-<h1 class="hidden">Projects</h1>
-<p>These are some of my personal projects and explorations</p>
+<h1 class="hidden">Thoughts</h1>
