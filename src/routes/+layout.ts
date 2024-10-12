68dee89b3233183a8dcfce56dce430b542cc6936
arm-out <arminsuraj@gmail.com>
2024-10-13 05:56:46 +0800

    layout data fetch

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,19 @@
 export const prerender = true;
+
+export const load = async ({ url }) => {
+	// Project Count
+	const projects = import.meta.glob('/src/projects/*.md');
+	const projectCount = Object.keys(projects).length;
+
+	// Age Calculation
+	const dob = new Date('2001-08-24');
+	const today = new Date();
+	let age = today.getFullYear() - dob.getFullYear();
+	const monthDifference = today.getMonth() - dob.getMonth();
+	const dayDifference = today.getDate() - dob.getDate();
+	if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
+		age--;
+	}
+
+	return { projectCount, age, url: url.pathname };
+};
