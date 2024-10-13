f1cc44ccffbd70567cb1a5046fe6ef4b30f7e149
arm-out <arminsuraj@gmail.com>
2024-10-13 09:02:58 +0800

    featured projects fade animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,45 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import Currently from './Currently.svelte';
 	import FeaturedProject from './FeaturedProject.svelte';
+	import { navigating } from '$app/stores';
+	import { browser } from '$app/environment';
 
 	export let data;
+
+	let isInternalNavigation = false;
+	let external = false;
+	onMount(() => {
+		const unsubscribe = navigating.subscribe(($navigating) => {
+			if ($navigating) {
+				// This is an internal navigation
+				isInternalNavigation = true;
+			} else {
+				// Navigation completed or it's the initial page load
+				if (!isInternalNavigation) {
+					// This is either the initial page load or an external navigation
+					const referrer = document.referrer;
+					const currentDomain = window.location.hostname;
+
+					if (referrer && new URL(referrer).hostname === currentDomain) {
+						// console.log('Navigated from another page within the domain');
+					} else {
+						// console.log('Navigated from an external source or direct access');
+						external = true;
+					}
+				} else {
+					// console.log('Completed internal navigation');
+				}
+
+				// Reset for next navigation
+				isInternalNavigation = false;
+			}
+		});
+
+		return () => {
+			unsubscribe();
+		};
+	});
 </script>
 
 <svelte:head>
@@ -27,32 +64,44 @@
 
 <h3 class="text-sec mt-8 mb-4">Featured Projects</h3>
 
-<FeaturedProject
-	title="SeeFood"
-	desc="is an AI nuitrition assistant that provides you with macro breakdowns from pictures of your meals"
-	live="https://seefoodai.vercel.app/"
-	code="https://github.com/arm-out/seefood"
-	href="/seefood"
-/>
+{#if browser}
+	{#key external}
+		<FeaturedProject
+			title="SeeFood"
+			desc="is an AI nuitrition assistant that provides you with macro breakdowns from pictures of your meals"
+			live="https://seefoodai.vercel.app/"
+			code="https://github.com/arm-out/seefood"
+			href="/seefood"
+			i={0}
+			{external}
+		/>
 
-<FeaturedProject
-	title="NES"
-	desc="is a fully functioning NES emulator written in Rust"
-	code="https://github.com/arm-out/seefood"
-	href="/nes"
-/>
+		<FeaturedProject
+			title="NES"
+			desc="is a fully functioning NES emulator written in Rust"
+			code="https://github.com/arm-out/seefood"
+			href="/nes"
+			i={1}
+			{external}
+		/>
 
-<FeaturedProject
-	title="StageManager"
-	desc="is a remote websocket based stage management system built for TEDxUCSD"
-	code="https://github.com/arm-out/seefood"
-	href="/seefood"
-/>
+		<FeaturedProject
+			title="StageManager"
+			desc="is a remote websocket based stage management system built for TEDxUCSD"
+			code="https://github.com/arm-out/seefood"
+			href="/seefood"
+			i={2}
+			{external}
+		/>
 
-<FeaturedProject
-	title="ChartDuels"
-	desc="is a single player browser game of higher or lower based on Spotify streaming data"
-	live="https://www.chartduels.com/"
-	code="https://github.com/arm-out/ChartDuels"
-	href="/chartduels"
-/>
+		<FeaturedProject
+			title="ChartDuels"
+			desc="is a single player browser game of higher or lower based on Spotify streaming data"
+			live="https://www.chartduels.com/"
+			code="https://github.com/arm-out/ChartDuels"
+			href="/chartduels"
+			i={3}
+			{external}
+		/>
+	{/key}
+{/if}
