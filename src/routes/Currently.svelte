6c8112c23818303f4294885702215e46882dece4
arm-out <arminsuraj@gmail.com>
2024-10-14 17:04:12 +0800

    loading and error indicators

--- src/routes/Currently.svelte
+++ src/routes/Currently.svelte
@@ -8,7 +8,11 @@
 <p>
 	<span>Currently listening to </span>
 	{#await spotify}
-		<span>Loading...</span>
+		<span class="animate-pulse w-20 inline-block bg-[#1f1f1f]" aria-hidden="true">&nbsp;</span>
+		<span class="sr-only">Loading...</span>
+		<span> by </span>
+		<span class="animate-pulse w-20 inline-block bg-[#1f1f1f]" aria-hidden="true">&nbsp;</span>
+		<span class="sr-only">Loading...</span>
 	{:then song}
 		<a
 			class="underline decoration-arm decoration-1 underline-offset-[3px] hover:bg-arm bg-arm/30"
@@ -16,14 +20,18 @@
 			target="_blank">{song.name}</a
 		>
 		<span> by {song.artist}</span>
-	{:catch error}
-		<span class="text-red-500">Error: {error.message}</span>
+	{:catch}
+		<span class="text-red-500">excuses by my spotify endpoint for erroring</span>
 	{/await}
 
 	<span> and slowly reading </span>
 
 	{#await literal}
-		<span>Loading...</span>
+		<span class="animate-pulse w-20 inline-block bg-[#1f1f1f]" aria-hidden="true">&nbsp;</span>
+		<span class="sr-only">Loading...</span>
+		<span> by </span>
+		<span class="animate-pulse w-20 inline-block bg-[#1f1f1f]" aria-hidden="true">&nbsp;</span>
+		<span class="sr-only">Loading...</span>
 	{:then book}
 		<a
 			href={book.link}
@@ -31,7 +39,7 @@
 			target="_blank">{book.title}</a
 		>
 		<span> by {book.author}</span>
-	{:catch error}
-		<span class="text-red-500">Error: {error.message}</span>
+	{:catch}
+		<span>why my book endpoint errored</span>
 	{/await}
 </p>
