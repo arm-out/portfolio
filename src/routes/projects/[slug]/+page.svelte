e21ba0619cc5e93f31b82f6d3eb49a00bdacae99
arm-out <arminsuraj@gmail.com>
2024-10-15 04:59:20 +0800

    dynamic og generation cf worker

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	export let data;
+
+	console.log($page.url);
+	let ogImg =
+		'http://localhost:8787/og?title=' +
+		encodeURIComponent(data.meta.title) +
+		'&desc=' +
+		encodeURIComponent(data.meta.description);
 </script>
 
 <svelte:head>
 	<title>{data.meta.title}</title>
-	<meta name="description" content={data.meta.desc} />
+	<meta name="description" content={data.meta.description} />
+
+	<meta property="og:image" content={ogImg} />
+	<link rel="og-image-for-prerender" href={ogImg} />
 </svelte:head>
 
 <svelte:component this={data.content} />
