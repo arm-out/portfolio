c9f4462499cd51d1757c7d60b3aae3fd84137121
arm-out <arminsuraj@gmail.com>
2024-10-24 15:19:35 +0800

    enhanced img support

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { enhancedImages } from 'mdsvex-enhanced-images';
 import { mdsvex } from 'mdsvex';
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOpts = {
-	extensions: ['.md']
+	extensions: ['.md'],
+	remarkPlugins: [enhancedImages]
 };
 
 /** @type {import('@sveltejs/kit').Config} */
