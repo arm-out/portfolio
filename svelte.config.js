9df3044bde2d778d6447e7f7612b421d9bc0fddf
arm-out <arminsuraj@gmail.com>
2024-10-31 22:05:19 +0800

    syntax highlighting

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,32 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { enhancedImages } from 'mdsvex-enhanced-images';
-import { mdsvex } from 'mdsvex';
+import { mdsvex, escapeSvelte } from 'mdsvex';
+import { createHighlighter } from 'shiki';
+import shikiColorizedBrackets from '@michael-makes/shiki-colorized-brackets';
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOpts = {
 	extensions: ['.md'],
 	layout: {
 		_: './src/mdsvex.svelte'
+	},
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await createHighlighter({
+				themes: ['material-theme-darker'],
+				langs: ['ts']
+			});
+			const html = escapeSvelte(
+				highlighter.codeToHtml(code, {
+					lang,
+					theme: 'material-theme-darker',
+					colorReplacements: { '#f07178': '#82aaff', '#212121': '#121212' },
+					transformers: [shikiColorizedBrackets()]
+				})
+			);
+			return `{@html \`${html}\`}`;
+		}
 	},
 	remarkPlugins: [enhancedImages]
 };
